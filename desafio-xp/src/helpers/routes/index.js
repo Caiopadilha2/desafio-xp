5e54b383a5bdc87e23b3aff2b49d600f5d0bc604
Caio <caiopadilha@id.uff.br>
2022-07-18 20:08:38 -0300

    feat: páginas de depósito e saque, e página de notfound

--- desafio-xp/src/helpers/routes/index.js
+++ desafio-xp/src/helpers/routes/index.js
@@ -1,7 +1,9 @@
 import React, { Component } from 'react';
 import { Switch, Route } from 'react-router-dom';
+import Balance from '../../pages/Balance';
 import Login from '../../pages/Login';
 import Negociation from '../../pages/Negociation';
+import NotFound from '../../pages/NotFound';
 import Wallet from '../../pages/Wallet';
 
 class Routes extends Component {
@@ -11,8 +13,8 @@ class Routes extends Component {
         <Route exact path="/" component={ Login } />
         <Route path="/wallet" component={ Wallet } />
         <Route path="/negociation" component={ Negociation } />
-        <Route path="/balance" component={ } />
-        <Route path="/*" component= {  } />
+        <Route path="/balance" component={ Balance } />
+        <Route path="/*" component={ NotFound } />
       </Switch>
     );
   }
