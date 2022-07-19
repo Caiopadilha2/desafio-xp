a85d8bfe1e5aa7572f54865793120687a2c9c84b
Caio <caiopadilha@id.uff.br>
2022-07-19 20:04:25 -0300

    test: login redireciona para página de wallet

--- desafio-xp/src/tests/Login.test.js
+++ desafio-xp/src/tests/Login.test.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { screen } from '@testing-library/react';
+// import userEvent from '@testing-library/user-event';
 import renderWithRouter from './renderWithRouter';
 import Login from '../components/LoginComponent';
 
@@ -20,3 +21,11 @@ test('Botão está na tela.', () => {
   const button = screen.getByRole('button', { name: 'Acessar' });
   expect(button).toBeInTheDocument();
 });
+
+// test('Botão de acessar redireciona para página Wallet.', () => {
+//   renderWithRouter(<Login />);
+//   const button = screen.getByRole('button', { name: 'Acessar' });
+//   userEvent.click(button);
+//   const title = screen.getByRole('heading', { name: 'Minhas ações', level: 3 });
+//   expect(title).toBeInTheDocument();
+// });
