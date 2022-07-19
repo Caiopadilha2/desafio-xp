e92207f6ed8c786e4dee20a31a5622e0640ed639
Caio <caiopadilha@id.uff.br>
2022-07-19 01:01:08 -0300

    feat: página para depósito/saque, botões, input e componente header

--- desafio-xp/src/components/BalanceComponent.jsx
+++ desafio-xp/src/components/BalanceComponent.jsx
@@ -1,9 +1,54 @@
 import React from 'react';
+import { useHistory } from 'react-router-dom';
+import HeaderComponent from './HeaderComponent';
 
-const BalanceComponent = () => (
-  <div>
-    <h3> Depósito e retirada.</h3>
-  </div>
-);
+const BalanceComponent = () => {
+  const history = useHistory();
+  return (
+    <div>
+      <HeaderComponent />
+      <section>
+        <p>Saldo em conta: R$500,00</p>
+        <button
+          type="button"
+          // onClick={  }
+        >
+          Depositar
+
+        </button>
+        <button
+          type="button"
+          // onClick={ }
+        >
+          Sacar
+
+        </button>
+        <div>
+          <input
+            placeholder="Informe o valor"
+            type="number"
+            min="0"
+            // value={ }
+          />
+        </div>
+        <button
+          type="button"
+          onClick={ () => history.push('/wallet') }
+        >
+          Voltar
+
+        </button>
+        <button
+          type="button"
+          // onClick={  }
+        >
+          Confirmar
+
+        </button>
+      </section>
+
+    </div>
+  );
+};
 
 export default BalanceComponent;
