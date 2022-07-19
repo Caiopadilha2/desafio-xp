2492cb8dfd5127060ceedf0432fc6d4682f9ec21
Caio <caiopadilha@id.uff.br>
2022-07-19 20:10:17 -0300

    test: Título ações disponíveis aparece na tela de wallet

--- desafio-xp/src/tests/Wallet.test.js
+++ desafio-xp/src/tests/Wallet.test.js
@@ -8,3 +8,9 @@ test('Título de minhas ações aparece na tela.', () => {
   const heading = screen.getByRole('heading', { name: 'Minhas ações', level: 3 });
   expect(heading).toBeInTheDocument();
 });
+
+test('Título de ações disponíveis aparece na tela.', () => {
+  renderWithRouter(<Wallet />);
+  const heading = screen.getByRole('heading', { name: /Disponíveis/i, level: 3 });
+  expect(heading).toBeInTheDocument();
+});
