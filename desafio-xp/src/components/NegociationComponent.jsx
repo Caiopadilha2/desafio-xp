66ed740fea30722361fbcc76c2cff8c21d932eed
Caio <caiopadilha@id.uff.br>
2022-07-18 23:49:50 -0300

    refactor: coloca as informações do componente wallet no lugar correto e retira do balance. Adiciona redirecionamento nos botões de comprar e vender

--- desafio-xp/src/components/NegociationComponent.jsx
+++ desafio-xp/src/components/NegociationComponent.jsx
@@ -1,70 +1,7 @@
 import React from 'react';
-import { useHistory } from 'react-router-dom';
-import { myStocks, StocksToBy } from '../data';
-import HeaderComponent from './HeaderComponent';
 
-const NegociationComponent = () => {
-  const history = useHistory();
-
-  return (
-    <div>
-      <HeaderComponent />
-      <h3>Minhas ações</h3>
-      <table>
-        <thead>
-          <tr>
-            <th>Ação</th>
-            <th>Qtde.</th>
-            <th>Valor (R$)</th>
-            <th>Negociar</th>
-          </tr>
-        </thead>
-
-        <tbody>
-          { myStocks.map(({ id, name, amount, value }) => (
-            <tr key={ id } id={ id }>
-              <td>{name}</td>
-              <td>{amount}</td>
-              <td>{`R$ ${value},00`}</td>
-            </tr>
-          ))}
-        </tbody>
-
-      </table>
-
-      <h3>Disponíveis para investir</h3>
-      <table>
-        <thead>
-          <tr>
-            <th>Ação</th>
-            <th>Qtde.</th>
-            <th>Valor (R$)</th>
-            <th>Negociar</th>
-          </tr>
-        </thead>
-
-        <tbody>
-          { StocksToBy.map(({ id, name, amount, value }) => (
-            <tr key={ id } id={ id }>
-              <td>{name}</td>
-              <td>{amount}</td>
-              <td>{`R$ ${value},00`}</td>
-            </tr>
-          ))}
-        </tbody>
-
-      </table>
-      <button
-        type="button"
-        onClick={ () => history.push('/balance') }
-      >
-        Depósito/Retirada
-
-      </button>
-    </div>
-  );
-};
+const NegociationComponent = () => (
+  <h3>Negociar</h3>
+);
 
 export default NegociationComponent;
-
-// refatorar pra ficar em uma função só.
