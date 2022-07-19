a3d25fbe8ea95bb7579231279f25525639994ab1
Caio <caiopadilha@id.uff.br>
2022-07-19 19:36:24 -0300

    test: input de email na tela

--- desafio-xp/src/tests/Login.test.js
+++ desafio-xp/src/tests/Login.test.js
@@ -3,6 +3,12 @@ import { screen } from '@testing-library/react';
 import renderWithRouter from './renderWithRouter';
 import Login from '../components/LoginComponent';
 
+test('Input de email está na tela.', () => {
+  renderWithRouter(<Login />);
+  const input = screen.getByPlaceholderText('Email:');
+  expect(input).toBeInTheDocument();
+});
+
 test('Botão está na tela.', () => {
   renderWithRouter(<Login />);
   const button = screen.getByRole('button', { name: 'Acessar' });
