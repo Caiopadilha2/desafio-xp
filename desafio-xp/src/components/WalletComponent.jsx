e5af16b6cfd8720f412d5d3889f65bf19ae03ab6
Caio <caiopadilha@id.uff.br>
2022-07-23 20:26:24 -0300

    refactor: componentiza navbar

--- desafio-xp/src/components/WalletComponent.jsx
+++ desafio-xp/src/components/WalletComponent.jsx
@@ -6,6 +6,7 @@ import olhoAberto from '../assets/olho_aberto.jpeg';
 import olhoFechado from '../assets/olho_fechado.jpeg';
 import HeaderComponent from './HeaderComponent';
 import { clearUserLocalStorage } from '../helpers/LocalStorage';
+import NavBar from './NavBar';
 
 const WalletComponent = () => {
   const history = useHistory();
@@ -42,37 +43,11 @@ const WalletComponent = () => {
     setEyeOpen(!eyeOpen);
   };
 
-  const logout = () => {
-    clearUserLocalStorage();
-    history.push('/');
-  };
-
   return (
     <div className="bg-zinc-800 border-2 rounded-2xl max-w-lg py-16 px-12">
       <div>
         <HeaderComponent />
-        <nav className="flex justify-around underline bg-sky-600 font-black border-2">
-          <a
-            href="https://blog.xpeducacao.com.br/categoria/como-investir/?gclid=Cj0KCQjwuO6WBhDLARIsAIdeyDL_7R5dGtBXIW7jfJXfwhnMybj19couN9DRLo5lPC2kekEQML1pPwoaAtPwEALw_wcB"
-            target="_blank"
-            rel="noreferrer"
-          >
-            Aprenda a investir!
-          </a>
-          <a
-            href="https://www.gov.br/cvm/pt-br/canais_atendimento/consultas-reclamacoes-denuncias"
-            target="_blank"
-            rel="noreferrer"
-          >
-            Fale com a CVM
-          </a>
-          <a
-            href="/"
-            onClick={ () => logout() }
-          >
-            Logout
-          </a>
-        </nav>
+        <NavBar />
         <input
           type="image"
           alt="olho"
